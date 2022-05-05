282b904d4f0defe324973f7db4b66d77ffd7a9f1
Amaranese <alejandromaranes@gmail.com>
2022-05-05 10:27:09 +0200

    Create td3.py

---
+++ resources/td3.py
@@ -0,0 +1,84 @@
+training_doc3 = """
+Margie even wrote about it that night in her diary. On the page headed May 17, 2155, she wrote, Today Tommy found a real book!
+
+It was a very old book. Margie’s grandfather once said that when he was a little boy his grandfather told him that there was a time when all stories were printed on paper.
+
+They turned the pages, which were yellow and crankily, and it was awfully funny to read words that stood still instead of moving the way they were supposed to – on a screen, you know. And then, when they turned back to the page before, it had the same words on it that it had had when they read it the first time.
+
+Gee, said Tommy, what a waste. When you’re though with the book, you just throw it away, I guess. Our television screen must have had a million books on it and it’s good for plenty more. I wouldn’t throw it away.
+
+Same with mine, said Margie. She was eleven and hadn’t seen as many telebooks as Tommy had. He was thirteen.
+
+She said, Where did you find it?
+
+In my house. He pointed without looking, because he was busy reading. In the attic.
+
+What’s it about?
+
+School.
+
+Margie was scornful. School? What’s there to write about school? I hate school. Margie had always hated school, but now she hated it more than ever. The mechanical teacher had been giving her test after test in geography and she had been doing worse and worse until her mother had shaken her head sorrowfully and sent for the County Inspector.
+
+He was a round little man with a red face and a whole box of tools with dials and wires. He smiled at her and gave her an apple, then took the teacher apart. Margie had hoped he wouldn’t know how to put it together again, but he knew how all right and, after an hour or so, there it was again, large and black and ugly with a big screen on which all the lessons were shown and the questions were asked. That wasn’t so bad. The part she hated the most was the slot where she had to put homework and test papers. She always had to write them out in a punch code they made her learn when she was six years old, and the mechanical teacher calculated the mark in no time.
+
+The inspector had smiled after he was finished and patted her head. He said to her mother, It’s not the little girl’s fault, Mrs. Jones. I think the geography sector was geared a little too quick. Those things happen sometimes. I’ve slowed it up to an average ten-year level. Actually, the over-all pattern of her progress is quite satisfactory. And he patted Margie’s head again.
+
+Margie was disappointed. She had been hoping they would take the teacher away altogether. They had once taken Tommy’s teacher away for nearly a month because the history sector had blanked out completely.
+
+So she said to Tommy, Why would anyone write about school?
+
+Tommy looked at her with very superior eyes. Because it’s not our kind of school, stupid. This is the old kind of school that they had hundreds and hundreds of years ago.
+
+Margie was hurt. Well, I don’t know what kind of school they had all that time ago. She read the book over his shoulder for a while, then said, Anyway, they had a teacher.
+
+Sure they had a teacher, but it wasn’t a regular teacher. It was a man.
+
+A man. How could a man be a teacher?
+
+Well, he just told the boys and girls things and gave them homework and asked them questions.
+
+A man isn’t smart enough.
+
+Sure he is. My father knows as much as my teacher.
+
+He can’t. A man can’t know as much as a teacher.
+
+He knows almost as much I betcha.
+
+Margie wasn’t prepared to dispute that. She said, I wouldn’t want a strange man in my house to teach me.
+
+Tommy screamed with laughter. You don’t know much, Margie. The teachers didn’t live in the house. They had a special building and all the kids went there.
+
+And all the kids learned the same thing?
+
+Sure, if they were the same age.
+
+But my mother says a teacher has to be adjusted to fit the mind of each boy and girl it teaches and that each kid has to be taught differently.
+
+Just the same, they didn’t do it that way then. If you don’t like it, you don’t have to read the book.
+
+I didn’t say I didn’t like it, Margie said quickly. She wanted to read about those funny schools.
+
+They weren’t nearly half finished when Margie’s mother called, Margie! School!
+
+Margie looked up. Not yet, mamma.
+
+Now, said Mrs. Jones. And it’s probably time for Tommy, too.
+
+Margie said to Tommy, Can I read the book some more with you after school?
+
+Maybe, he said, nonchalantly. He walked away whistling, the dusty old book tucked beneath his arm.
+
+Margie went to the schoolroom. It was right next to her bedroom, and the mechanical teacher was on and waiting for her. It was always on at the same time every day except for Saturday and Sunday, because her mother said little girls learned better if they learned at regular hours.
+
+The screen was lit up, and it said: Today’s arithmetical lesson is on the addition of proper fractions. Please insert yesterday’s homework in the proper slot.
+
+Margie did so with a sigh. She was thinking about the old schools they had when her grandfather’s grandfather was a boy. All the kids from the whole neighborhood came, laughing and shouting in the school yard, sitting together in the schoolroom, going home together at the end of the day. They learned the same things so they could help one another on the homework and talk about it.
+
+And the teachers were people…
+
+The mechanical teacher was flashing on the screen. When we add the fractions ½ and ¼ …
+
+Margie was thinking about how the kids must have loved it in the old days. She was thinking about the fun they had.
+
+"""
