2c7f02e768d273de76c1bf7d97b185d9ed5c7493
Amaranese <alejandromaranes@gmail.com>
2022-05-05 10:27:12 +0200

    Create td2.py

---
+++ resources/td2.py
@@ -0,0 +1,150 @@
+training_doc2 = """
+My name is Joe. That is what my colleague, Milton Davidson,
+calls me. He is a programmer and I am a computer program. I am
+part of the Multivac-complex and am connected with other parts
+all over the world. I know everything. Almost everything.
+ I am Milton's private program. His Joe. He understands more
+about programming than anyone in the world, and I am his
+experimental model. He has made me speak better than any
+other computer can.
+ "It is just a matter of matching sounds to symbols, Joe," he
+told me. "That's the way it works in the human brain even though
+we still don't know what symbols there are in the brain. I know
+the symbols in yours, and I can match them to words, one-toone." So I talk. I don't think I talk as well as I think, but Milton
+says I talk very well. Milton has never married, though he is
+nearly forty years old. He has never found the right woman, he
+told me. One day he said, "I'll find her yet, Joe. I'm going to
+find the best. I'm going to have true love and you're going to
+help me. I'm tired of improving you in order to solve the
+problems of the world. Solve my problem. Find me true love."
+ I said, "What is true love?"
+ "Never mind. That is abstract. Just find me the ideal girl. 
+You are connected to the Multivac-complex so you can reach the
+data banks of every human being in the world. We'll eliminate
+them all by groups and classes until we're left with only one
+person. The perfect person. She will be for me."
+I said, "I am ready."
+ He said, "Eliminate all men first."
+ It was easy. His words activated symbols in my molecular
+valves. I could reach out to make contact with the accumulated
+data on every human being in the world. At his words, I withdrew
+from 3,784,982,874 men. I kept contact with 3,786,112,090
+women.
+He said, "Eliminate all younger than twenty-five; all older than
+forty. Then eliminate all with an IQ under 120; all with a height
+under 150 centimeters and over 175 centimeters."
+ He gave me exact measurements; he eliminated women with
+living children; he eliminated women with various genetic
+characteristics. "I'm not sure about eye color," he said. "Let
+that go for a while. But no red hair. I don't like red hair."
+ After two weeks, we were down to 235 women. They all spoke
+English very well. Milton said he didn't want a language problem.
+Even computer-translation would get in the way at intimate
+moments.
+ "I can't interview 235 women," he said. "It would take too 
+much time, and people would discover what I am doing."
+ "It would make trouble," I said. Milton had arranged me to do
+things I wasn't designed to do. No one knew about that.
+ "It's none of their business," he said, and the skin on his face
+grew red. "I tell you what, Joe, I will bring in holographs, and you
+check the list for similarities."
+ He brought in holographs of women. "These are three beauty
+contest winners," he said. "Do any of the 235 match?"
+ Eight were very good matches and Milton said, "Good, you
+have their data banks. Study requirements and needs in the job
+market and arrange to have them assigned here. One at a time,
+of course." He thought a while, moved his shoulders up and down,
+and said, "Alphabetical order."
+ That is one of the things I am not designed to do. Shifting
+people from job to job for personal reasons is called
+manipulation. I could do it now because Milton had arranged it. I
+wasn't supposed to do it for anyone but him, though.
+ The first girl arrived a week later. Milton's face turned red
+when he saw her. He spoke as though it were hard to do so. They
+were together a great deal and he paid no attention to me. One
+time he said, "Let me take you to dinner."
+ The next day he said to me, "It was no good, somehow. There
+was something missing. She is a beautiful woman, but I didn't 
+feel any touch of true love. Try the next one."
+ It was the same with all eight. They were much alike. They
+smiled a great deal and had pleasant voices, but Milton always
+found it wasn't right. He said, "I can't understand it, Joe. You
+and I have picked out the eight women who, in all the world, look
+the best to me. They are ideal. Why don't they please me?"
+ I said, "Do you please them?"
+ His eyebrows moved and he pushed one fist hard against his
+other hand. "That's it, Joe. It's a two-way street. If I am not
+their ideal, they can't act in such a way as to be my ideal. I must
+be their true love, too, but how do I do that?" He seemed to be
+thinking all that day.
+ The next morning he came to me and said, "I'm going to leave
+it to you, Joe. All up to you. You have my data bank, and I am
+going to tell you everything I know about myself. You fill up my
+data bank in every possible detail but keep all additions to
+yourself " “What will I do with the data bank, then, Milton?"
+"Then you will match it to the 235 women. No, 227. Leave out
+the eight you've seen. Arrange to have each undergo a
+psychiatric examination. Fill up their data banks and compare
+them with mine. Find correlations." (Arranging psychiatric
+examinations is another thing that is against my original
+instructions.)
+ For weeks, Milton talked to me. He told me of his parents and
+his siblings. He told me of his childhood and his schooling and his
+adolescence. He told me of the young women he had admired
+from a distance. His data bank grew and he adjusted me to
+broaden and deepen my symbol-taking.
+ He said, “You see, Joe, as you get more and more of me in
+you, I adjust you to match me better and better. You get to
+think more like me, so you understand me better. If you
+understand me well enough, then any woman, whose data bank is
+something you understand as well, would be my true love." He
+kept talking to me and I came to understand him better and
+better.
+ I could make longer sentences and my expressions grew more
+complicated. My speech began to sound a good deal like his in
+vocabulary, word order and style. I said to him one time, "You
+see, Milton, it isn't a matter of fitting a girl to a physical ideal
+only. You need a girl who is a personal, emotional, temperamental
+fit to you. If that happens, looks are secondary. If we can't find
+the fit in these 227, we'll look elsewhere. We will find someone
+who won't care how you look either, or how anyone would look, if
+only there is the personality fit. What are looks?"
+ "Absolutely," he said. "I would have known this if I had had
+more to do with women in my life. Of course, thinking about it 
+makes it all plain now."
+ We always agreed; we thought so like each other. "We
+shouldn't have any trouble, now, Milton, if you'd let me ask you
+questions. I can see where, in your data bank, there are blank
+spots and unevenesses." What followed, Milton said, was the
+equivalent of a careful psychoanalysis. Of course. I was learning
+from the psychiatric examinations of the 227 women-on all of
+which I was keeping close tabs.
+ Milton seemed quite happy. He said, "Talking to you, Joe, is
+almost like talking to another self. Our personalities have come
+to match perfectly!"
+ "So will the personality of the woman we choose."
+ For I had found her and she was one of the 227 after all. Her
+name was Charity Jones and she was an Evaluator at the Library
+of History in Witchita. Her extended data bank fit ours
+perfectly. All the other women had fallen into discard in one
+respect or another as the data banks grew fuller, but with
+Charity there was increasing and astonishing resonance.
+ I didn't have to describe her to Milton. Milton had
+coordinated my symbolism so closely with his own I could tell the
+resonance directly. It fit me. Next it was a matter of adjusting
+the work sheets and job requirements in such a way as to get
+Charity assigned to us. It must be done very delicately, so no one 
+would know that anything illegal had taken place.
+ Of course, Milton himself knew, since it was he who arranged
+it and that had to be taken care of too. When they came to
+arrest him on grounds of malfeasance in office, it was,
+fortunately, for something that had taken place ten years ago.
+He had told me about it, of course, so it was easy to arrange-and
+he won't talk about me for that would make his offense much
+worse.
+ He's gone, and tomorrow is February 14. Valentine's Day.
+Charity will arrive then with her cool hands and her sweet voice.
+I will teach her how to operate me and how to care for me. What
+do looks matter when our personalities will resonate?
+ I will say to her, "I am Joe, and you are my true love."
+"""
