d6c099b6705395a946b1f1fbf63397859c584be9
Amaranese <alejandromaranes@gmail.com>
2022-05-05 10:27:01 +0200

    Create chat.py

---
+++ twitter-project/chat.py
@@ -0,0 +1,4 @@
+import numpy as np
+import re
+from test_model import encoder_model, decoder_model, num_decoder_tokens, num_encoder_tokens, input_features_dict, target_features_dict, reverse_target_features_dict, max_decoder_seq_length, max_encoder_seq_length
+
